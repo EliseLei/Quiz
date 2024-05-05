cdd20d35a5379d1280d22b2f517940b032cfb94a
EliseLei <eliseleininger@gmail.com>
2024-05-05 17:30:26 +0200

    added the data with a json

--- script/script.js
+++ script/script.js
@@ -1,19 +1,45 @@
-function checkInput() {
-   $(".check").click(function(event){
-        event.preventDefault();
-        const form = $(this).closest("form");
-        const input = form.find(".form-input");
+function questions() {
+   const questionsData = $("#questionsData").text();
+   const questions = JSON.parse(questionsData);
+   
+   questions.forEach(function(question) {
+      const form = $("<form>"); 
+      const questionLabel = $("<label>").text(question.question);
 
-        if (!input[0].checkValidity()) {
-            input.removeClass('validStyle'); 
-            input.addClass('errorStyle'); 
-        } else {
-            input.removeClass('errorStyle');
-            input.addClass('validStyle'); 
-        }
-    });
+      const input = $("<input>").attr({
+         type: "text",
+         pattern: question.pattern, 
+         class: "text-slate-950 form-input"
+      });
+
+      const submitBtn = $("<input>").attr({
+         type: "submit",
+         class: "check"
+      });
+
+      form.append(questionLabel, "<br>", input, submitBtn);
+
+      $("#questionsSection").append(form);
+   });
 }
 
- $(document).ready(function(){
-    checkInput();
- });
+function checkInput() {
+   $(".check").click(function(event){
+      event.preventDefault();
+      const form = $(this).closest("form");
+      const input = form.find(".form-input");
+
+      if (!input[0].checkValidity()) {
+         input.removeClass('validStyle'); 
+         input.addClass('errorStyle'); 
+      } else {
+         input.removeClass('errorStyle');
+         input.addClass('validStyle'); 
+      }
+   });
+}
+
+$(document).ready(function(){
+   questions();
+   checkInput();
+});
