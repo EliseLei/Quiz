c9a1c18e37a14efacbe52ad60195c88d4e385b34
EliseLei <eliseleininger@gmail.com>
2024-05-05 18:17:33 +0200

    add another question + update style

--- script/script.js
+++ script/script.js
@@ -4,20 +4,25 @@ function questions() {
    
    questions.forEach(function(question) {
       const form = $("<form>"); 
+      const div = $("<div>").attr({
+         class: "flex justify-center"
+      });
+
       const questionLabel = $("<label>").text(question.question);
 
       const input = $("<input>").attr({
          type: "text",
          pattern: question.pattern, 
-         class: "text-slate-950 form-input"
+         class: "text-slate-950 form-input h-10"
       });
 
-      const submitBtn = $("<input>").attr({
+      const submitBtn = $("<button>").attr({
          type: "submit",
-         class: "check"
-      });
+         class: "check btn",
+      }).text("Check");;
 
-      form.append(questionLabel, "<br>", input, submitBtn);
+      div.append(input, submitBtn);
+      form.append(questionLabel, "<br>", div);
 
       $("#questionsSection").append(form);
    });
