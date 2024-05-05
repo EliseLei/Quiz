59163961e61555eaff5723cb8f21b5a901690031
EliseLei <eliseleininger@gmail.com>
2024-05-06 00:28:07 +0200

    added a help

--- script/getQuestions.js
+++ script/getQuestions.js
@@ -13,23 +13,36 @@ function getQuestions() {
        const input = $("<input>").attr({
           type: "text",
           pattern: question.pattern, 
-          class: "text-slate-950 form-input h-10 border-white border-2"
+          class: "text-slate-950 form-input h-10 border-white border-2 w-full"
        });
- 
+
+       const clue = $('<div>').addClass("text-white");
+       const clueButton = $('<div>').addClass("clue-btn cursor-pointer").text("See clue");
+       const clueText = $('<span>').addClass("clue-text hidden").text(question.help);
+      
+       showClue(clueButton, clueText);
+       clue.append(clueButton, clueText);
+
        const submitBtn = $("<button>").attr({
           type: "submit",
           class: "check btn",
        }).text("Check");;
  
        div.append(input, submitBtn);
-       form.append(questionLabel, "<br>", div);
+       form.append(questionLabel, clue, "<br>", div);
  
        $("#questionsSection").append(form);
     });
+
  }
 
+ function showClue(clueButton, clueText) {
+   clueButton.click(function() {
+      clueText.toggleClass("hidden");
+   });
+}
 
  $(document).ready(function(){
-    getQuestions();
+   getQuestions();
  });
  
