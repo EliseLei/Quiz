c1d45aec48da6fe90c2b0fb3dff3f8bfc0b00ff7
EliseLei <eliseleininger@gmail.com>
2024-05-05 15:51:56 +0200

    added a valid and error style

--- script/script.js
+++ script/script.js
@@ -3,15 +3,13 @@ function checkInput() {
         event.preventDefault();
         const form = $(this).closest("form");
         const input = form.find(".form-input");
-        const inputValue = input.val();
-        console.log(input);
-        console.log(inputValue);
+
         if (!input[0].checkValidity()) {
-            input.removeClass('border-emerald-700 bg-emerald-400'); 
-            input.addClass('border-red-700 bg-red-400'); 
+            input.removeClass('validStyle'); 
+            input.addClass('errorStyle'); 
         } else {
-            input.removeClass('border-red-700 bg-red-400');
-            input.addClass('border-emerald-700 bg-emerald-400'); 
+            input.removeClass('errorStyle');
+            input.addClass('validStyle'); 
         }
     });
 }
